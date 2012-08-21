ba6aef1b6fcd63ee429e0685d8fd7845b864e4c2
Peter Huang <bugwei@gmail.com>
2012-08-21 14:23:25 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HenleySite::Application.config.secret_token = '6d95d12c1c917a92679a70299f4ab3e945c2698a90acacb157692da62947bf85e3102e703b4cad0693e5d35d39684e93d73374871a2f81a716db612f7ba7bba4'
